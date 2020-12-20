cede2dfa471658a8bb077d04034f51a6317d7b34
Zhenxi <PiscesXP@outlook.com>
2020-12-20 19:25:23 +0800

    添加存货确认指令

--- src/main/kotlin/cn/itxia/chatbot/util/CommandWords.kt
+++ src/main/kotlin/cn/itxia/chatbot/util/CommandWords.kt
@@ -4,13 +4,15 @@ class CommandWords {
 
     companion object {
 
-        val YUQUE_SEARCH = listOf("doc", "yuque", "语雀", "羽雀")
+        val YUQUE_SEARCH = listOf("doc", "yuque", "yqdoc", "语雀", "羽雀")
 
         val REPEATER = listOf("复读机", "复读")
 
         val HELP_MANUAL = listOf("help")
 
         val WEBSITE_LINKS = listOf("links", "link", "常用网站", "常用网址", "常用链接")
+
+        val ALIVE_CHECK = listOf("存活确认")
     }
 
 }
