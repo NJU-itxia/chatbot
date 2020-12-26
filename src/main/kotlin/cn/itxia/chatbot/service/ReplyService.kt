f98d00949c7e5bf8b9a7bbce254be4c00a119666
Zhenxi <PiscesXP@outlook.com>
2020-12-27 01:13:27 +0800

    增加直接返回消息的callback

--- src/main/kotlin/cn/itxia/chatbot/service/ReplyService.kt
+++ src/main/kotlin/cn/itxia/chatbot/service/ReplyService.kt
@@ -13,20 +13,55 @@ class ReplyService {
 
     private val messageProcessServiceList: MutableList<MessageProcessService> = mutableListOf()
 
+    /**
+     * 处理发来的消息.
+     * 有回复时调用reply函数返回消息.
+     *
+     * 为什么不直接return，而是增加一个reply函数？
+     * 因为有些消息处理可能需要较长时间，如果等待所有消息再return会造成延迟.
+     * 直接调用reply函数则可以及时返回消息.
+     * (当然，你还可以选择另一个return MessageList的overload)
+     *
+     * @param incomingMessage 发来的消息
+     * @param reply 回复的回调
+     * */
+    fun replyMessage(incomingMessage: IncomingMessage, reply: (ResponseMessage) -> Unit) {
+
+        for (messageProcessService in messageProcessServiceList) {
+
+            //交给service处理消息
+            val result = messageProcessService.process(incomingMessage)
+
+            //回复消息
+            result.response.forEach { reply(it) }
+
+            //判断是否该执行后续的service
+            if (!result.shouldContinueProcess) {
+                break
+            }
+
+        }
+
+    }
+
+
+    /**
+     * 上一个方法的overload.
+     *
+     * @param incomingMessage 发来的消息
+     * @return 回复消息的list
+     * */
     fun replyMessage(incomingMessage: IncomingMessage): List<ResponseMessage> {
+
 
         val responseMessageList: MutableList<ResponseMessage> = mutableListOf()
 
-        //交给service处理消息
-        for (messageProcessService in messageProcessServiceList) {
-            val processResult = messageProcessService.process(incomingMessage)
-            responseMessageList.addAll(processResult.response)
-            if (!processResult.shouldContinueProcess) {
-                break
-            }
+        val collectToList: (ResponseMessage) -> Unit = fun(responseMessage: ResponseMessage) {
+            responseMessageList.add(responseMessage)
         }
 
-        //不返回消息(这个部分设计可能不太好)
+        replyMessage(incomingMessage, collectToList)
+
         return responseMessageList
     }
 
