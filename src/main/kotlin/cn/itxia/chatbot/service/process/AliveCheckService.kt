ba578f28ca4cd7908fe480c4c2a07733b961192c
Zhenxi <PiscesXP@outlook.com>
2021-01-01 12:22:55 +0800

    存活确认指令

---
+++ src/main/kotlin/cn/itxia/chatbot/service/process/AliveCheckService.kt
@@ -0,0 +1,21 @@
+package cn.itxia.chatbot.service.process
+
+import cn.itxia.chatbot.message.incoming.IncomingMessage
+import cn.itxia.chatbot.service.process.ProcessResult.Companion.reply
+import cn.itxia.chatbot.util.CommandWords
+import org.springframework.stereotype.Service
+
+/**
+ * 存活确认.
+ * 看看机器人是不是挂掉了.
+ */
+@Service
+class AliveCheckService : CommandProcessService() {
+    override fun shouldExecute(commandName: String, isExplicitCall: Boolean, isArgumentEmpty: Boolean): Boolean {
+        return isExplicitCall && CommandWords.ALIVE_CHECK.contains(commandName)
+    }
+
+    override fun executeCommand(argument: String, isExplicitCall: Boolean, message: IncomingMessage): ProcessResult {
+        return reply("啊，我还活着", true)
+    }
+}
