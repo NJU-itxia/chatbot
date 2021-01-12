73f5131bf5f2f1acae99e7d1a0bedfdc5b263c58
Zhenxi <PiscesXP@outlook.com>
2021-01-12 23:19:17 +0800

    修改注释

--- src/main/kotlin/cn/itxia/chatbot/message/Command.kt
+++ src/main/kotlin/cn/itxia/chatbot/message/Command.kt
@@ -1,13 +1,23 @@
 package cn.itxia.chatbot.message
 
+/**
+ * 指令.
+ * */
 class Command private constructor(
     val commandName: String,
     val argument: String,
 ) {
 
+    /**
+     * 参数是否为空.
+     * (0长度，或者全为空白)
+     * */
     val isArgumentEmpty: Boolean = argument.isEmpty()
 
     companion object {
+        /**
+         * 从文本中解析出指令.
+         * */
         fun fromText(text: String): Command? {
             val command = text.trim()
             if (command.isEmpty()) {
--- src/main/kotlin/cn/itxia/chatbot/service/message/AbstractCommandProcessService.kt
+++ src/main/kotlin/cn/itxia/chatbot/service/message/AbstractCommandProcessService.kt
@@ -5,12 +5,12 @@ import cn.itxia.chatbot.message.ProcessResult
 import cn.itxia.chatbot.message.incoming.IncomingMessage
 
 /**
- * 处理空格分隔命令式消息.
+ * 处理空格分隔指令式消息.
  *
  * 例如消息是"doc p.nju", 可以看作是指令"doc", 参数"p.nju".
  * 意为用关键字"p.nju"来查询文档.
  *
- * 如果消息不符合命令式格式, 则会调用onNotExecute, 该函数默认交给下一个service执行.
+ * 如果消息不符合指令格式, 则会调用onNotExecute, 该函数默认交给下一个service执行.
  * */
 abstract class AbstractCommandProcessService : AbstractMessageProcessService() {
 
@@ -31,7 +31,7 @@ abstract class AbstractCommandProcessService : AbstractMessageProcessService() {
     abstract fun executeCommand(command: Command, message: IncomingMessage): ProcessResult
 
     /**
-     * 不执行命令则会调用这个函数.
+     * 不执行指令则会调用这个函数.
      *
      * 默认是交给下一个service执行.
      * @param message 原始消息
@@ -41,7 +41,7 @@ abstract class AbstractCommandProcessService : AbstractMessageProcessService() {
     }
 
     /**
-     * 处理命令.
+     * 处理指令.
      * */
     final override fun process(message: IncomingMessage): ProcessResult {
         val command = message.commandStyle ?: return onNotExecute(message)
